dff9db5830a703090bd383470fd96ccc3e614e08
tranchien2002 <chie.tm2002@gmail.com>
2019-10-27 17:14:25 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import login from './torus-login.svg';
 import logo from './logo.svg';
+import getWeb3Torus from './getWeb3Torus';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  login = async () => {
+    const web3 = await getWeb3Torus();
+    const accounts = await web3.eth.getAccounts();
+    alert(accounts);
+  };
+  render() {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>Torus demo</p>
+          <div>
+            <div className='container-custom'>
+              <img alt='' src={login} onClick={this.login} className='gif-load' />
+            </div>
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
